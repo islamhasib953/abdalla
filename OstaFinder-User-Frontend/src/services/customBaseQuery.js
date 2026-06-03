e922f8a78bafaf36944c03d40846dbe31ad49831
islamhasib953 <islamhasib953@gmail.com>
2026-06-04 00:57:10 +0300

    Fix: removed submodules and merged successfully

--- OstaFinder-User-Frontend/src/services/customBaseQuery.js
+++ OstaFinder-User-Frontend/src/services/customBaseQuery.js
@@ -2,7 +2,7 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { getCookie } from '../utils/cookies';
 
-const productionURL = import.meta.env.VITE_API_URL
+const productionURL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export const customBaseQuery = fetchBaseQuery({
   baseUrl: productionURL,
